c076185d8c49d74767e883360b06cd63ab7de41a
=Mohamed Amin <=mohamedaminaa213@gmail.com>
2023-10-12 17:55:19 +0100

    added for.while.do..while. if/else/of. switch and arrays done by heart

---
+++ js_challenges.js
@@ -0,0 +1,53 @@
+// write a for loop that prints your name 20 times
+
+// git add .
+// git commit -m "whatever changes you've made."
+// git push 
+
+// for (let i = 0; i < 20; i++) {
+//     console.log("Mohamed");
+// }
+
+// write a while loop that prints your name 20 times
+
+// let i = 0;
+// while ( i < 20 ) {
+//     console.log("Amin")
+//     i++
+// }
+
+// convert the while loop above to do while loop
+
+// let i = 0; 
+// do {
+//     console.log("Amin")
+//     i++
+// }
+// while (false) 
+
+// write an array that hold 10 names of your friends and family
+
+// let friendsAndFamily = ["Mohamed","Ali","A"]
+
+// write a code that chekcks if today is Monday, it should say go to school
+// else if today tuesday go to work
+// else if today friday go to the mosque
+
+const today = "Monday"
+
+
+
+
+// convert the if/else/if program above switch
+
+switch (today) {
+    case "Monday":
+        console.log("Go to school froms switch")
+         break;
+     case "Tuesday":
+        console.log("Go to work from switch")    
+    break;
+    default:
+        console.log("Go to the mosque from switch")
+        break;
+}
